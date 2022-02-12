8518756db704deaf3056653483b12200729d00d9
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-02-11 22:30:05 -0600

    add mainnet address

--- constants/index.ts
+++ constants/index.ts
@@ -2,6 +2,6 @@ import { SupportedChainId } from 'types'
 
 // todo: use the real addresses
 export const VALENFTINES_ADDRESS = {
-  [SupportedChainId.MAINNET]: '0xeAe95cBE33D6bE0f3839dd5bb920EC3E319819CF',
+  [SupportedChainId.MAINNET]: '0xe6bDDCA6e786De76C5AfD47c6913e696798281F2',
   [SupportedChainId.RINKEBY]: '0xeAe95cBE33D6bE0f3839dd5bb920EC3E319819CF',
 }
