d4b73192f0dcf920d513e6c89489c7f9331ce80b
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-02-11 22:03:15 -0600

    scroll hint on heart picker modal

--- components/CloseButton/index.tsx
+++ components/CloseButton/index.tsx
@@ -2,7 +2,7 @@ import styles from './CloseButton.module.scss'
 
 export default function CloseButton({ onClick }: { onClick: () => void }) {
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={styles.button} onClick={onClick} id="modal-close-button">
       💔
     </button>
   )
